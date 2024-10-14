40ae25dce17732aa9a5e8c0144b60aec96297928
Alan-H-H <123487644+Alan-H-H@users.noreply.github.com>
2024-10-14 20:57:44 -0300

    Environmentals actualizacion

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-// Load environment variables (make sure to add them in your .env file)
-const supabaseUrl = 'https://kcughgqrtsqxgakbfarv.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjdWdoZ3FydHNxeGdha2JmYXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5Mzk5NDQsImV4cCI6MjA0NDUxNTk0NH0.rwWolyQEwfJgoUmLiGXTJ9DHdwAT1OWJOoyFHprQ83M';
+// Load environment variables
+dotenv.config();
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 // Initialize the Supabase client
 export const supabase = createClient(supabaseUrl, supabaseKey);
